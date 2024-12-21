19d7155434d525c0202093e8b23bc475ef4f57c6
lhes23 <32542078+lhes23@users.noreply.github.com>
2024-12-21 22:03:55 +0800

    rename dynamo name

--- dynamodb-table/main.tf
+++ dynamodb-table/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = var.dynamodb_table_name
+  name         = var.name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
--- dynamodb-table/variables.tf
+++ dynamodb-table/variables.tf
@@ -1,4 +1,4 @@
-variable "dynamodb_table_name" {
+variable "name" {
   description = "The name of the DynamoDB table for state locking."
   type        = string
   default     = "terraform-lock"
